c8f877334f747a371f44991f85ccf693c218c327
grago1999 <grago1999@gmail.com>
2016-06-18 13:16:10 -0700

    1.0.1
    Comment descriptions for implementation

--- VRKit/VRKit/ViewController.swift
+++ VRKit/VRKit/ViewController.swift
@@ -13,11 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Sample implementation
+        
+        
+        // Create a VRView object, which will take up the full screen size by default
+        
         let vrView = VRView()
         self.view.addSubview(vrView)
         
+        // Add UI objects to the VRView the same way as you would to a UIView
+        
         let myView = UIView(frame:CGRect(x:20, y:20, width:40, height:40))
         myView.backgroundColor = UIColor.redColor()
+        
+        // The only difference is use addVRSubview(UIView) in place of addSubview(UIView)
+        
         vrView.addVRSubview(myView)
     }
 
