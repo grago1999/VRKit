fae50201792c720e5584f9574ae052d7eaf134ae
grago1999 <grago1999@gmail.com>
2016-07-04 23:03:10 -0400

    1.0.2
    - Basic UIImageView support
    - Began working on continuous view support

--- VRKit/VRKit/VRView.swift
+++ VRKit/VRKit/VRView.swift
@@ -14,6 +14,8 @@ class VRView: UIView {
     private let screenWidth = UIScreen.mainScreen().bounds.size.width
     private let screenHeight = UIScreen.mainScreen().bounds.size.height
     
+    private var isContinuous:Bool = false
+    
     private var leftView:UIView
     private var rightView:UIView
     
@@ -22,9 +24,16 @@ class VRView: UIView {
     private var leftSubViews:[UIView]
     private var rightSubViews:[UIView]
     
+    private var leftMargin:CGFloat = 0.0
+    private var rightMargin:CGFloat = 0.0
+    private var topMargin:CGFloat = 0.0
+    private var bottomMargin:CGFloat = 0.0
+    
     init() {
         leftView = UIView(frame:CGRect(x:0, y:0, width:screenWidth/2, height:screenHeight))
+        leftView.layer.masksToBounds = true
         rightView = UIView(frame:CGRect(x:screenWidth/2, y:0, width:screenWidth/2, height:screenHeight))
+        rightView.layer.masksToBounds = true
         leftSubViews = []
         rightSubViews = []
         super.init(frame:CGRect(x:0, y:0, width:screenWidth, height:screenHeight))
@@ -39,19 +48,10 @@ class VRView: UIView {
             motionManager.gyroUpdateInterval = interval
             motionManager.startGyroUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: { (gyroData:CMGyroData?, error:NSError?) in
                 for vrSubView in self.getAllSubViews() {
-                    vrSubView.frame = CGRectOffset(vrSubView.frame, CGFloat(-(gyroData?.rotationRate.x)!), CGFloat(-(gyroData?.rotationRate.y)!))
+                    vrSubView.frame = CGRectOffset(vrSubView.frame, CGFloat((gyroData?.rotationRate.x)!), CGFloat(-(gyroData?.rotationRate.y)!))
                 }
             })
         }
-    }
-    
-    func addVRSubview(view:UIView) {
-        let leftSubView = view
-        let rightSubView = view.copyView() as! UIView
-        leftView.addSubview(leftSubView)
-        rightView.addSubview(rightSubView)
-        leftSubViews.append(leftSubView)
-        rightSubViews.append(rightSubView)
     }
     
     required init?(coder aDecoder:NSCoder) {
@@ -60,6 +60,57 @@ class VRView: UIView {
         leftSubViews = []
         rightSubViews = []
         super.init(coder:aDecoder)
+    }
+    
+    func setContinuous(value:Bool) {
+        isContinuous = value
+        alterContinuousViews()
+    }
+    
+    private func alterContinuousViews() {
+        if isContinuous {
+            for subView in leftSubViews {
+                if subView.frame.origin.x < leftMargin {
+                    leftMargin = subView.frame.origin.x
+                }
+                if subView.frame.origin.x+subView.frame.size.width > rightMargin {
+                    rightMargin = subView.frame.origin.x+subView.frame.size.width
+                }
+                if subView.frame.origin.y < topMargin {
+                    topMargin = subView.frame.origin.y
+                }
+                if subView.frame.origin.y+subView.frame.size.height > bottomMargin {
+                    bottomMargin = subView.frame.origin.y+subView.frame.size.height
+                }
+            }
+        } else {
+            leftMargin = 0.0
+            rightMargin = 0.0
+            topMargin = 0.0
+            bottomMargin = 0.0
+        }
+    }
+    
+    func addVRSubView(view:UIView) {
+        let leftSubView = view
+        let rightSubView = view.copyView() as! UIView
+        leftView.addSubview(leftSubView)
+        rightView.addSubview(rightSubView)
+        leftSubViews.append(leftSubView)
+        rightSubViews.append(rightSubView)
+        alterContinuousViews()
+    }
+    
+    func addVRSubImgView(imgView:UIImageView) {
+        let leftSubView = imgView
+        let tempImgView = UIImageView(frame:imgView.frame)
+        tempImgView.image = imgView.image
+        let rightSubView = tempImgView
+        leftView.addSubview(leftSubView)
+        rightView.addSubview(rightSubView)
+        leftSubViews.append(leftSubView)
+        rightSubViews.append(rightSubView)
+        alterContinuousViews()
     }
     
     private func getAllSubViews() -> [UIView] {
--- VRKit/VRKit/ViewController.swift
+++ VRKit/VRKit/ViewController.swift
@@ -10,25 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let screenWidth = UIScreen.mainScreen().bounds.size.width
+    let screenHeight = UIScreen.mainScreen().bounds.size.height
+    
+    // Sample implementation
+    
+    // Create a VRView object, which will take up the full screen size by default
+    
+    let vrView = VRView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Sample implementation
+        // Adding the VRView to the main view
         
-        
-        // Create a VRView object, which will take up the full screen size by default
-        
-        let vrView = VRView()
         self.view.addSubview(vrView)
         
-        // Add UI objects to the VRView the same way as you would to a UIView
+        // By default the VRView will not create a continuous view, meaning it will not wrap around as the user reaches the edge of the content
+        // You can enable it with the following function
+        
+        vrView.setContinuous(true)
+        
+        // Go to these functions to see implementation for certain UI elements
+        
+        addView()
+        
+        addImgView()
+    }
+    
+    // Make sure to consider the position of the element when adding any elements to the VRView
+    // This visible area for elements is half the screen width by the screen height, not the usual screen width by screen height
+    
+    func addView() {
+        // Add UIView objects to the VRView the same way as you normally would
         
         let myView = UIView(frame:CGRect(x:20, y:20, width:40, height:40))
         myView.backgroundColor = UIColor.redColor()
         
-        // The only difference is use addVRSubview(UIView) in place of addSubview(UIView)
+        // The only difference is use addVRSubView(UIView) in place of addSubview(UIView)
         
-        vrView.addVRSubview(myView)
+        vrView.addVRSubView(myView)
+    }
+    
+    func addImgView() {
+        // Add UIImageView objects to the VRView the same way as you normally would
+        
+        let img = UIImage(named:"img.jpg")
+        let imgView = UIImageView(frame:CGRect(x:0, y:0, width:img!.size.width/4, height:img!.size.height/4))
+        imgView.center = CGPoint(x:screenWidth/4, y:screenHeight/2)
+        imgView.image = img
+        
+        // The only difference is use addVRSubImgView(UIView) in place of addSubview(UIImageView)
+        
+        vrView.addVRSubImgView(imgView)
     }
 
     override func didReceiveMemoryWarning() {
